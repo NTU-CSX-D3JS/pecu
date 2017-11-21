aa3aabb233be3d27b6ee800e78752567c68be234
pecu <pecu610@gmail.com>
2017-11-21 18:31:03 +0800

    Update jsfunction.js

--- jsfunction.js
+++ jsfunction.js
@@ -1,5 +1,8 @@
 function myFunction(p1, p2) {
-    return ((p1 + p2) * (p1 - p2));
+    var show = "((" + p1.toString() + "+" + p1.toString() + ")*(" + p1.toString() + "-" + p2.toString() + ")) = ";
+    var ans = ((p1 + p2) * (p1 - p2));
+    var str = show + ans.toString();
+    return str
 }
 
 function d3select(id) {
